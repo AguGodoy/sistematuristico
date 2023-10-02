c03a2455fefca1689d6a4677ef3ede7936405e83
AguGodoy <agustinrx100@gmail.com>
2023-10-02 12:13:32 -0300

    actualizacion entidades

--- src/sistematuristico/Paquete.java
+++ src/sistematuristico/Paquete.java
@@ -12,7 +12,7 @@ public class Paquete {
     private int idPaquete;
     private Ciudad origen;
     private Ciudad destino;
-    private Alojamiento alojaminto;
+    private Alojamiento alojamiento;
     private Pasaje pasaje;
 
     public Paquete() {
@@ -21,9 +21,60 @@ public class Paquete {
     public Paquete(Ciudad origen, Ciudad destino, Alojamiento alojaminto, Pasaje pasaje) {
         this.origen = origen;
         this.destino = destino;
-        this.alojaminto = alojaminto;
+        this.alojamiento = alojaminto;
+        this.pasaje = pasaje;
+    }
+
+    public Paquete(int idPaquete, Ciudad origen, Ciudad destino, Alojamiento alojaminto, Pasaje pasaje) {
+        this.idPaquete = idPaquete;
+        this.origen = origen;
+        this.destino = destino;
+        this.alojamiento = alojaminto;
+        this.pasaje = pasaje;
+    }
+
+    public int getIdPaquete() {
+        return idPaquete;
+    }
+
+    public void setIdPaquete(int idPaquete) {
+        this.idPaquete = idPaquete;
+    }
+
+    public Ciudad getOrigen() {
+        return origen;
+    }
+
+    public void setOrigen(Ciudad origen) {
+        this.origen = origen;
+    }
+
+    public Ciudad getDestino() {
+        return destino;
+    }
+
+    public void setDestino(Ciudad destino) {
+        this.destino = destino;
+    }
+
+    public Alojamiento getAlojaminto() {
+        return alojamiento;
+    }
+
+    public void setAlojaminto(Alojamiento alojaminto) {
+        this.alojamiento = alojaminto;
+    }
+
+    public Pasaje getPasaje() {
+        return pasaje;
+    }
+
+    public void setPasaje(Pasaje pasaje) {
         this.pasaje = pasaje;
     }
     
-    
+    @Override
+    public String toString(){
+        return idPaquete+" "+origen+" "+destino+" " +alojamiento+ " "+ pasaje;
+    }
 }
