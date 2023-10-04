28d1dfc260b5730b7cb826c786020c97e08d7f2f
Cristian Garcia <cristian_garcia@outlook.com>
2023-10-04 12:36:46 -0300

    ABMCiudad Update

--- src/vistas/ABMCiudad.java
+++ src/vistas/ABMCiudad.java
@@ -34,10 +34,28 @@ public class ABMCiudad extends javax.swing.JInternalFrame {
         jLabel1 = new javax.swing.JLabel();
         jSeparator1 = new javax.swing.JSeparator();
         jSeparator2 = new javax.swing.JSeparator();
-
-        getContentPane().setLayout(new org.netbeans.lib.awtextra.AbsoluteLayout());
+        jScrollPane1 = new javax.swing.JScrollPane();
+        jTable1 = new javax.swing.JTable();
+        jTextField1 = new javax.swing.JTextField();
+        jLabel2 = new javax.swing.JLabel();
+        jLabel3 = new javax.swing.JLabel();
+        jTextField2 = new javax.swing.JTextField();
+        jLabel4 = new javax.swing.JLabel();
+        jTextField3 = new javax.swing.JTextField();
+        jLabel5 = new javax.swing.JLabel();
+        jTextField4 = new javax.swing.JTextField();
+        jLabel6 = new javax.swing.JLabel();
+        jTextField5 = new javax.swing.JTextField();
+        jLabel7 = new javax.swing.JLabel();
+        jCheckBox1 = new javax.swing.JCheckBox();
+        jButton1 = new javax.swing.JButton();
+        jButton2 = new javax.swing.JButton();
+        jButton3 = new javax.swing.JButton();
+        jCheckBox2 = new javax.swing.JCheckBox();
+        jButton4 = new javax.swing.JButton();
 
         jPanelFull.setBackground(new java.awt.Color(56, 63, 79));
+        jPanelFull.setLayout(new org.netbeans.lib.awtextra.AbsoluteLayout());
 
         jPanelHead.setBackground(new java.awt.Color(56, 63, 79));
 
@@ -76,28 +94,178 @@ public class ABMCiudad extends javax.swing.JInternalFrame {
                 .addContainerGap(14, Short.MAX_VALUE))
         );
 
-        javax.swing.GroupLayout jPanelFullLayout = new javax.swing.GroupLayout(jPanelFull);
-        jPanelFull.setLayout(jPanelFullLayout);
-        jPanelFullLayout.setHorizontalGroup(
-            jPanelFullLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addComponent(jPanelHead, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
-        );
-        jPanelFullLayout.setVerticalGroup(
-            jPanelFullLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addComponent(jPanelHead, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
-        );
+        jPanelFull.add(jPanelHead, new org.netbeans.lib.awtextra.AbsoluteConstraints(0, 0, 1000, -1));
 
-        getContentPane().add(jPanelFull, new org.netbeans.lib.awtextra.AbsoluteConstraints(0, 0, 1000, 640));
+        jTable1.setModel(new javax.swing.table.DefaultTableModel(
+            new Object [][] {
+                {null, null, null, null},
+                {null, null, null, null},
+                {null, null, null, null}
+            },
+            new String [] {
+                "Title 1", "Title 2", "Title 3", "Title 4"
+            }
+        ));
+        jScrollPane1.setViewportView(jTable1);
+
+        jPanelFull.add(jScrollPane1, new org.netbeans.lib.awtextra.AbsoluteConstraints(476, 288, 480, 88));
+
+        jTextField1.setText("jTextField1");
+        jPanelFull.add(jTextField1, new org.netbeans.lib.awtextra.AbsoluteConstraints(610, 200, 169, 38));
+
+        jLabel2.setForeground(new java.awt.Color(235, 237, 255));
+        jLabel2.setText("Nombre Ciudad:");
+        jPanelFull.add(jLabel2, new org.netbeans.lib.awtextra.AbsoluteConstraints(480, 200, 110, 38));
+
+        jLabel3.setForeground(new java.awt.Color(235, 237, 255));
+        jLabel3.setText("Id de Ciudad");
+        jPanelFull.add(jLabel3, new org.netbeans.lib.awtextra.AbsoluteConstraints(24, 192, 110, 38));
+
+        jTextField2.setText("jTextField1");
+        jPanelFull.add(jTextField2, new org.netbeans.lib.awtextra.AbsoluteConstraints(152, 192, 169, 38));
+
+        jLabel4.setForeground(new java.awt.Color(235, 237, 255));
+        jLabel4.setText("Nombre Ciudad:");
+        jPanelFull.add(jLabel4, new org.netbeans.lib.awtextra.AbsoluteConstraints(24, 248, 110, 38));
+
+        jTextField3.setText("jTextField1");
+        jPanelFull.add(jTextField3, new org.netbeans.lib.awtextra.AbsoluteConstraints(152, 248, 169, 38));
+
+        jLabel5.setForeground(new java.awt.Color(235, 237, 255));
+        jLabel5.setText("Nombre Provincia:");
+        jPanelFull.add(jLabel5, new org.netbeans.lib.awtextra.AbsoluteConstraints(24, 304, 110, 38));
+
+        jTextField4.setText("jTextField1");
+        jPanelFull.add(jTextField4, new org.netbeans.lib.awtextra.AbsoluteConstraints(152, 304, 169, 38));
+
+        jLabel6.setForeground(new java.awt.Color(235, 237, 255));
+        jLabel6.setText("Nombre Pais:");
+        jPanelFull.add(jLabel6, new org.netbeans.lib.awtextra.AbsoluteConstraints(24, 365, 110, 38));
+
+        jTextField5.setText("jTextField1");
+        jPanelFull.add(jTextField5, new org.netbeans.lib.awtextra.AbsoluteConstraints(152, 365, 169, 38));
+
+        jLabel7.setForeground(new java.awt.Color(235, 237, 255));
+        jLabel7.setText("Estado:");
+        jPanelFull.add(jLabel7, new org.netbeans.lib.awtextra.AbsoluteConstraints(24, 421, 110, 38));
+
+        jCheckBox1.setText("jCheckBox1");
+        jCheckBox1.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                jCheckBox1ActionPerformed(evt);
+            }
+        });
+        jPanelFull.add(jCheckBox1, new org.netbeans.lib.awtextra.AbsoluteConstraints(152, 421, 169, 38));
+
+        jButton1.setFont(new java.awt.Font("Dialog", 1, 14)); // NOI18N
+        jButton1.setText("Nuevo");
+        jButton1.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                jButton1ActionPerformed(evt);
+            }
+        });
+        jPanelFull.add(jButton1, new org.netbeans.lib.awtextra.AbsoluteConstraints(152, 489, 130, 45));
+
+        jButton2.setFont(new java.awt.Font("Dialog", 1, 14)); // NOI18N
+        jButton2.setText("Eliminar");
+        jButton2.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                jButton2ActionPerformed(evt);
+            }
+        });
+        jPanelFull.add(jButton2, new org.netbeans.lib.awtextra.AbsoluteConstraints(353, 489, 130, 45));
+
+        jButton3.setFont(new java.awt.Font("Dialog", 1, 14)); // NOI18N
+        jButton3.setText("Modificar");
+        jButton3.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                jButton3ActionPerformed(evt);
+            }
+        });
+        jPanelFull.add(jButton3, new org.netbeans.lib.awtextra.AbsoluteConstraints(518, 489, 130, 45));
+
+        jCheckBox2.setText("Mostrar Eliminados");
+        jCheckBox2.setHorizontalAlignment(javax.swing.SwingConstants.CENTER);
+        jCheckBox2.setHorizontalTextPosition(javax.swing.SwingConstants.LEFT);
+        jCheckBox2.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                jCheckBox2ActionPerformed(evt);
+            }
+        });
+        jPanelFull.add(jCheckBox2, new org.netbeans.lib.awtextra.AbsoluteConstraints(800, 200, 160, 40));
+
+        jButton4.setFont(new java.awt.Font("Dialog", 1, 14)); // NOI18N
+        jButton4.setText("Guardar");
+        jButton4.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                jButton4ActionPerformed(evt);
+            }
+        });
+        jPanelFull.add(jButton4, new org.netbeans.lib.awtextra.AbsoluteConstraints(689, 489, 130, 45));
+
+        javax.swing.GroupLayout layout = new javax.swing.GroupLayout(getContentPane());
+        getContentPane().setLayout(layout);
+        layout.setHorizontalGroup(
+            layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addComponent(jPanelFull, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
+        );
+        layout.setVerticalGroup(
+            layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addComponent(jPanelFull, javax.swing.GroupLayout.PREFERRED_SIZE, 640, javax.swing.GroupLayout.PREFERRED_SIZE)
+        );
 
         pack();
     }// </editor-fold>//GEN-END:initComponents
 
+    private void jButton4ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jButton4ActionPerformed
+        // TODO add your handling code here:
+    }//GEN-LAST:event_jButton4ActionPerformed
+
+    private void jCheckBox2ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jCheckBox2ActionPerformed
+        // TODO add your handling code here:
+    }//GEN-LAST:event_jCheckBox2ActionPerformed
+
+    private void jButton3ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jButton3ActionPerformed
+        // TODO add your handling code here:
+    }//GEN-LAST:event_jButton3ActionPerformed
+
+    private void jButton2ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jButton2ActionPerformed
+        // TODO add your handling code here:
+    }//GEN-LAST:event_jButton2ActionPerformed
+
+    private void jButton1ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jButton1ActionPerformed
+        // TODO add your handling code here:
+    }//GEN-LAST:event_jButton1ActionPerformed
+
+    private void jCheckBox1ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jCheckBox1ActionPerformed
+        // TODO add your handling code here:
+    }//GEN-LAST:event_jCheckBox1ActionPerformed
+
 
     // Variables declaration - do not modify//GEN-BEGIN:variables
+    private javax.swing.JButton jButton1;
+    private javax.swing.JButton jButton2;
+    private javax.swing.JButton jButton3;
+    private javax.swing.JButton jButton4;
+    private javax.swing.JCheckBox jCheckBox1;
+    private javax.swing.JCheckBox jCheckBox2;
     private javax.swing.JLabel jLabel1;
+    private javax.swing.JLabel jLabel2;
+    private javax.swing.JLabel jLabel3;
+    private javax.swing.JLabel jLabel4;
+    private javax.swing.JLabel jLabel5;
+    private javax.swing.JLabel jLabel6;
+    private javax.swing.JLabel jLabel7;
     private javax.swing.JPanel jPanelFull;
     private javax.swing.JPanel jPanelHead;
+    private javax.swing.JScrollPane jScrollPane1;
     private javax.swing.JSeparator jSeparator1;
     private javax.swing.JSeparator jSeparator2;
+    private javax.swing.JTable jTable1;
+    private javax.swing.JTextField jTextField1;
+    private javax.swing.JTextField jTextField2;
+    private javax.swing.JTextField jTextField3;
+    private javax.swing.JTextField jTextField4;
+    private javax.swing.JTextField jTextField5;
     // End of variables declaration//GEN-END:variables
 }
