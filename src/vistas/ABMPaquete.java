160d4c7be2e42c4ae5057fc1491f9fc8366fa10e
Cristian Garcia <cristian_garcia@outlook.com>
2023-10-24 10:42:20 -0300

    Boton de la Esquina izquierda arriba listo

--- src/vistas/ABMPaquete.java
+++ src/vistas/ABMPaquete.java
@@ -2,7 +2,7 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  * Click nbfs://nbhost/SystemFileSystem/Templates/GUIForms/JInternalFrame.java to edit this template
  */
-package vistas.sobras;
+package vistas;
 import sistematuristico.AccesoData.PasajeData;
 import sistematuristico.Entidades.Pasaje;
 
